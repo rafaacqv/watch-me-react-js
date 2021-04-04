4e2b1c36b89a5f7456f4fcc987cbc527b9b054e4
Rafael Araujo <rafaacq@gmail.com>
2021-04-03 23:35:21 -0300

    Add movies custom hook

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,23 @@
+import { useMovies } from '../hooks/useMovies';
+import { Button } from '../components/Button';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, handleClickButton, selectedGenreId } = useMovies();
+  
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
